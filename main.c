53087f881c7a3fc5771b9ceec7029ca4164e37f0
Clay <clayw@udel.edu>
2023-03-03 12:35:22 -0500

    debugging frees

--- main.c
+++ main.c
@@ -53,14 +53,14 @@ void matrixOneBigArray(int n, int m){
     }
 
     printf("\n");
-/*
+
     for(int i = 0; i<m; i++){
         free(ptr[i]);
     }
 
     free(ptr);
-    free(big);
-*/
+    //free(big);
+
 }
 
 int main(int argc, char** argv){
