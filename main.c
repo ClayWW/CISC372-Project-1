54cd834af58a66da14bf4eba397c0892d9111889
Clay <clayw@udel.edu>
2023-03-03 12:53:31 -0500

    testing

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ void matrixOneBigArray(int n, int m){
     */
     for(int j = 0; j < n; j++){
         for(int i = 0; i < m; i++){
-            printf("%f",big[(i*n)]);
+            printf("%f",big[(i*n)+j]);
             printf("\t");
         }
         printf("\n");
